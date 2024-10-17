c2456fea10fac364f6d54903beb053678db8d5bd
JorgeDiaz <jorgedaviddiaz62@gmail.com>
2024-10-17 10:49:41 -0500

    hola

---
+++ EjemploClase0202/src/ejemploclase0202/EjemploClase0202.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemploclase0202;
+
+/**
+ *
+ * @author utpl
+ */
+public class EjemploClase0202 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Trabajando en Neatbeans");
+        
+                
+                
+   
+    
+        
+    }
+
+}
